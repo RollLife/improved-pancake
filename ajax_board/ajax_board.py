8faef75560f5ae86cb761ac99a0985bab131ccc2
RollLife <qudgh7145@naver.com>
2016-10-20 15:34:55 +0900

    comment half complete

--- ajax_board/ajax_board.py
+++ ajax_board/ajax_board.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 ajax = []
+comment_Array = []
 
 # This route will show a form to perform an AJAX request
 # jQuery is loaded to execute the request and update the
@@ -13,12 +14,15 @@ def index():
 	print ajax
 	return render_template('index.html')
 
+
 @app.route('/insert')
 def insert_form():
 	return render_template('insert.html')
 
 @app.route('/add', methods=['POST'])
 def ajax_add():
+	comment_Array.append([])
+
 	one_by_one = []
 
 	title = request.form['title']
@@ -36,6 +40,7 @@ def ajax_add():
 
 @app.route('/get_list', methods=['GET'])
 def ajax_show():
+	print ajax
 	if ajax is None:
 		title = ""
 		writer = ""
@@ -60,8 +65,45 @@ def show_detail():
 
 @app.route('/view_list')
 def detail_option():
-	return jsonify({'array':ajax})
+	return jsonify({'array':ajax,'comment_Array':comment_Array})
 
+@app.route('/delete')
+def delete_detail():
+	num = int(request.args['id'])
+	del ajax[num]
+	print num
+	return redirect(url_for('index'))
+
+@app.route('/update')
+def update_insert():
+	return render_template('update.html')
+
+@app.route('/action', methods=['GET'])
+def update_detail():
+	num = request.args['id']
+	print num
+	return redirect(url_for('index'))
+
+@app.route('/comment', methods=['GET'])
+def comment_insert():
+	print "test text"
+	num = int(str(request.args['id']))
+	title = request.args['title']
+	content = request.args['content']
+	one_by_one = []
+	one_by_one.append(title)
+	one_by_one.append(content)
+	comment_Array[num].append(one_by_one)
+
+
+	# if request.method == 'POST':
+	# 	title = request.form['comment_title']
+	# 	content = request.form['comment_content']
+	# comment_Array.append(title)
+	# comment_Array.append(content)
+
+	print comment_Array
+	return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug=True)
